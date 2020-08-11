84ff2f7afd371b57aad1fa622127756d51419282
Logan Scott <logantscott@gmail.com>
2020-08-11 08:55:13 -0600

    seeding

---
+++ __tests__/goal.test.js
@@ -0,0 +1,10 @@
+require('../data-helpers/data-helpers');
+const { agent, prepare } = require('../data-helpers/data-helpers');
+
+const Goal = require('../lib/models/Goal');
+
+describe('goal routes', () => {
+  it('can create a goal', async() => {
+
+  });
+});
---
+++ data-helpers/seed.js
@@ -0,0 +1,36 @@
+const chance = require('chance').Chance();
+
+const User = require('../lib/models/User');
+const Goal = require('../lib/models/Goal');
+const Measure = require('../lib/models/Measure');
+const Datapoint = require('../lib/models/Datapoint');
+
+module.exports = async({ users = 20, goals = 100, measures = 300, datapoints = 1000 } = {}) => {
+
+  const createdUsers = await User.create([...Array(users)].map(() => ({
+    name: chance.name(),
+    email: chance.email({ domain: 'email.com' }),
+    password: chance.string()
+  })));
+
+  const createdGoals = await Goal.create([...Array(goals)].map(() => ({
+    name: chance.sentence(),
+    user: chance.pickone(createdUsers)._id
+  })));
+  
+  const createdMeasures = await Measure.create([...Array(measures)].map(() => {
+    const goal = chance.pickone(createdGoals);
+
+    return {
+      name: chance.word(),
+      user: goal.user,
+      goal: goal._id
+    };
+  }));
+
+  await Datapoint.create([...Array(datapoints)].map(() => ({
+    name: chance.bool({ likelihood: 75 }),
+    measure: chance.pickone(createdMeasures)._id
+  })));
+
+};
